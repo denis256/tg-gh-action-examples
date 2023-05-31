ba94409243d7ecd5fb484f322e562aff5eaef70d
Denis O <denis.o@linux.com>
2023-05-31 19:20:41 +0000

    Updated default content

--- simple-project/main.tf
+++ simple-project/main.tf
@@ -1,5 +1,11 @@
+variable "content" {}
+
 resource "local_file" "file" {
-  content         = "test"
+  content         = var.content
   filename        = "test.txt"
   file_permission = "0644"
+}
+
+output "file_name" {
+  value = local_file.file.filename
 }
